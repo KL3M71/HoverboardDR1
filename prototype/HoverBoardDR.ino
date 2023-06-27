d37f9a6708384590c9ca0e761271b0283d1d9af6
William Lim <william.lim781@gmail.com>
2023-06-27 00:27:28 -0500

    Create HoverBoardDR.ino
    just a mash-up of the motor control code and the audio processing code. This is a rough idea of what it could look like.
    
    just a side note but I think we might want to add a counter that counts the amount of time a certain frequency is detected. Doing this could help prevent the accidental triggering of the code due to noise.  Basically, we have  to give the microphone a certain frequency for a short time before it executes the command associated with the frequency

---
+++ prototype/HoverBoardDR.ino
@@ -0,0 +1,143 @@
+#include "arduinoFFT.h"
+
+arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
+
+#define CHANNEL A0
+const uint16_t samples = 64; //This value MUST ALWAYS be a power of 2
+const double samplingFrequency = 10000; //Hz, must be less than 10000 due to ADC
+
+unsigned int sampling_period_us;
+unsigned long microseconds;
+
+/*
+These are the input and output vectors
+Input vectors receive computed results from FFT
+*/
+
+double vReal[samples];
+double vImag[samples];
+
+bool digital = false; // digitalpin on microphone to detect when microphon is pickuping sound
+
+// Motor A connections
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+/* if we need a second motor we need to un comment said variables this applies to the other commnents in the setup() and
+   anywhere else these variables are used 
+// Motor B connections
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
+*/
+
+/*
+    is a flag for telling the motor which direction to turn
+   I just made a random convention for determining directions 
+   - 0 no turn 
+   - 1 clockwise
+   - -1 counter clockwise 
+*/
+int turnDirection = 0;
+
+void directionControl(int direction);
+
+void setup()
+{
+  sampling_period_us = round(1000000*(1.0/samplingFrequency));
+
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Ready");
+
+  pinMode(2,INPUT);
+
+	// Set all the motor control pins to outputs 
+	pinMode(enA, OUTPUT);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+  /*
+  pinMode(enB, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+	*/
+	// Turn off motors - Initial state
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+  /*
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+  */
+
+}
+
+void loop()
+{
+  /*SAMPLING*/
+  microseconds = micros();
+  digital = digitalRead(2);
+
+  for(int i=0; i<samples; i++)
+  {
+      vReal[i] = analogRead(CHANNEL);
+      vImag[i] = 0;
+      while(micros() - microseconds < sampling_period_us){
+        //empty loop
+      }
+      microseconds += sampling_period_us;
+  }
+  FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);	/* Weigh data */
+  FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
+  FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
+  double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
+  //Serial.println(x, 6); //Print out what frequency is the most dominant.
+
+
+  delay(50); /* Repeat after delay */
+  //Serial.println(digital); // used to get a glance at when the microphone is registering
+
+  
+  /* frequency detection for turning the hoverboard
+     digital is also used to help reduce the influence of noise in frequency detecting
+  */
+  if(x > 700 && x < 800 && digital)
+  {
+    Serial.println("this is the low frequency");
+    turnDirection = 1;
+    directionControl(turnDirection);
+  }
+
+  else if(x > 1200 && x < 1300 && digital)
+  {
+
+    Serial.println("this is the high frequency");
+    turnDirection = -1;
+    directionControl(turnDirection);
+  }
+
+  turnDirection = 0;
+}
+
+void directionControl(int direction){
+
+  // Set motors to maximum speed
+	// For PWM maximum possible values are 0 to 255
+	analogWrite(enA, 255);
+  switch(direction){
+    case -1:
+      	digitalWrite(in1, HIGH);
+	      digitalWrite(in2, LOW);
+      break;
+    case 1:
+        digitalWrite(in1, LOW);
+	      digitalWrite(in2, HIGH);
+      break; 
+  } 
+  delay(500);
+  
+  // turn off all motors
+
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+
+}
